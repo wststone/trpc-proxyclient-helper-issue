154c9dd7cd9fe160ae7d3393b22645934218bdaa
Rozstone <wangsiteng123@gmail.com>
2023-07-30 19:55:07 +0800

    correct url

--- src/pages/not-working.tsx
+++ src/pages/not-working.tsx
@@ -1,68 +1,73 @@
-import { createTRPCProxyClient, httpBatchLink } from '@trpc/client';
-import { createServerSideHelpers } from '@trpc/react-query/server';
+import { createTRPCProxyClient, httpBatchLink } from "@trpc/client";
+import { createServerSideHelpers } from "@trpc/react-query/server";
 import superjson from 'superjson';
-import { appRouter } from '~/server/routers/_app';
-import { getBaseUrl, trpc } from '~/utils/trpc';
+import { appRouter } from "~/server/routers/_app";
+import { getBaseUrl, trpc } from "~/utils/trpc";
+
+
+const correctedUrl = getBaseUrl() + '/api/trpc'
+
+console.log(correctedUrl)
 
 const proxyClient = createTRPCProxyClient<typeof appRouter>({
-  transformer: superjson,
-  links: [
-    httpBatchLink({
-      url: getBaseUrl(),
-    }),
-  ],
+	transformer: superjson,
+	links: [
+		httpBatchLink({
+			url: correctedUrl
+		}),
+	],
 });
 
 const helpers = createServerSideHelpers({
-  client: proxyClient,
+	client: proxyClient,
 });
-
 
 /**
  * This page will be served statically
  * @link https://trpc.io/docs/ssg
  */
 export const getStaticProps = async () => {
-  // helpers
+	// helpers
 
-  await helpers.greeting.prefetch({ name: 'client' });
+	await helpers.greeting.prefetch({ name: "client" });
 
-  const data = helpers.dehydrate()
+	const data = helpers.dehydrate();
 
-  console.log(data)
+	// console.log(data, data.queries[0]?.state);
 
-  return {
-    props: {
-      trpcState: data,
-    },
-    revalidate: 1,
-  };
+	return {
+		props: {
+			trpcState: data,
+		},
+		revalidate: 1,
+	};
 };
 
 export default function IndexPage() {
-  const result = trpc.greeting.useQuery({ name: 'client' });
+	const result = trpc.greeting.useQuery({ name: "client" });
 
-  if (!result.data) {
-    /* unreachable, page is served statically */
-    return (
-      <div style={styles}>
-        <h1>Loading...</h1>
-      </div>
-    );
-  }
+	if (!result.data) {
+		console.log("this should not happen")
+		/* unreachable, page is served statically */
+		return (
+			<div style={styles}>
+				<h1>Loading...</h1>
+			</div>
+		);
+	}
 
-  return (
-    <div style={styles}>
-      <h1>{result.data.text}</h1>
-      <p>{result.data.date.toDateString()}</p>
-    </div>
-  );
+	return (
+		<div style={styles}>
+			<h1>{result.data.text}</h1>
+			<p>{result.data.date.toDateString()}</p>
+		</div>
+	);
 }
 
 const styles = {
-  width: '100vw',
-  height: '100vh',
-  display: 'flex',
-  justifyContent: 'center',
-  alignItems: 'center',
+	width: "100vw",
+	height: "100vh",
+	display: "flex",
+	justifyContent: "center",
+	alignItems: "center",
 };
